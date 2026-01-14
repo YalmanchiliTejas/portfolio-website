67032cc43172a61f283579d887035bddd54bbb65
YalmanchiliTejas <tyalaman@purdue.edu>
2026-01-14 13:24:55 +0530

    added the papers

--- src/components/Papers.jsx
+++ src/components/Papers.jsx
@@ -13,7 +13,16 @@ import {
 } from '@mui/material';
 import { PictureAsPdf, ArrowOutward } from '@mui/icons-material';
 
-const papers = [];
+const papers = [
+{
+  title: 'Reimplementing PyTorch Cycle GANs for Art Style to Art Style Conversion',
+  authors: 'Tejas Yalamanchili, Shrinivas Venkatesan',
+  year: '2025',
+  abstract: `Cycle GAN in Pytorch specifically for art-style translation, constricting the dataset to paintings across multiple styles and evaluating photo to art and art to art conversions within this focused domain. In addition to reproducing the original objective from the paper (LSGAN with cycle and identity losses), we will explore replacing the LSGAN adversarial component with alternative formulations such as hinge loss, while also incorporating additional regularization terms including perceptual content loss (LPIPS/VGG), Gram-matrix style loss, and discriminator feature matching loss. Our goal is to evaluate how this modified objective influences style fidelity, realism, and content preservation in the translated images`,
+  topics: ['CVD', 'GANs'],
+  pdf: '../../public/CS593_project_final_report.pdf'
+},
+];
 
 const Papers = () => {
   return (
